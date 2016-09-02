82e332d8b0a4ecf3963a52d902964a7033aae7e4
James Park-Watt <jparkwatt@skyscapecloud.com>
2016-09-02 15:00:27 +0100

    Changed the way puppet is installed on the beaker host
    We need to install puppet from the puppet packages
    in order to get version 4 avaulable which is what
    the module is written in.

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -3,7 +3,13 @@ require 'pry'
 
 # Install Puppet on all hosts
 hosts.each do |host|
-  install_puppet_on(host)
+  # Debian only currently
+  on(host, 'apt-get --purge remove -y puppet*')
+  on(host, 'curl -o /tmp/puppet.deb http://apt.puppetlabs.com/puppetlabs-release-pc1-trusty.deb')
+  on(host, 'dpkg -i /tmp/puppet.deb')
+  install_puppet_agent_on(host)
+  add_aio_defaults_on(host)
+  add_puppet_paths_on(host)
 end
 
 RSpec.configure do |c|
@@ -15,12 +21,10 @@ RSpec.configure do |c|
     # Install module to all hosts
     hosts.each do |host|
       install_dev_puppet_module_on(host, :source => module_root, :module_name => 'dhcp',
-          :target_module_path => '/etc/puppet/modules')
+          :target_module_path => '/opt/puppetlabs/puppet/modules')
       # Install dependencies
       on(host, puppet('module', 'install', 'puppetlabs-concat'))
       on(host, puppet('module', 'install', 'puppetlabs-stdlib'))
-
-      # Add more setup code as needed
     end
   end
 end
