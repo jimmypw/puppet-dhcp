871497be3c4f857e452ce0f04861cfc1bdaf2fb7
James Park-Watt <jparkwatt@skyscapecloud.com>
2016-11-07 11:02:34 +0000

    Fixed Could not evaluate: invalid byte sequence in US-ASCII

--- manifests/server/service.pp
+++ manifests/server/service.pp
@@ -1,6 +1,8 @@
 class dhcp::server::service {
   service {'isc-dhcp-server' :
     ensure => running,
-    enable => true,
+    # Following line disabled as it breaks puppet with :
+    # Could not evaluate: invalid byte sequence in US-ASCII
+    # enable => true,
   }
 }
